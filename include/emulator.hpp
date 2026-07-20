ebc3f273d58e83aa505a66906c411d5d808534fc
Mustafa Gedikoglu <mustafae@uni-bremen.de>
2026-07-20 20:55:24 +0200

    Add Emulator interface with graphics/input stubs

---
+++ include/emulator.hpp
@@ -0,0 +1,8 @@
+class Emulator
+{
+private:
+public:
+    void setup_graphics();
+    void setup_input();
+    void draw_graphics();
+};
---
+++ src/emulator.cpp
@@ -0,0 +1,5 @@
+#include "emulator.hpp"
+
+void Emulator::setup_graphics() {};
+void Emulator::setup_input() {};
+void Emulator::draw_graphics() {};
